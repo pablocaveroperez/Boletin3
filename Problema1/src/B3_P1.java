3e851632078ff2fec8a568c91841139b7c6d42a0
Pablo Cavero Perez <gelves.mula@gmail.com>
2020-11-01 21:38:48 +0100

    Creadas las Inner Classes dentro de B3_P1

--- Problema1/src/B3_P1.java
+++ Problema1/src/B3_P1.java
@@ -1,2 +1,39 @@
 public class B3_P1 {
+    public class Control {
+
+    }
+
+    final Control control = new Control();
+
+    public class Hilo implements Runnable {
+        private int iId;
+
+        public int getiId() {
+            return iId;
+        }
+
+        public void setiId(int iId) {
+            this.iId = iId;
+        }
+
+        @Override
+        public synchronized void run() {
+
+        }
+    }
+
+    private void executeMultiThreading() {
+
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            B3_P1 oB3E8 = new B3_P1();
+            oB3E8.executeMultiThreading();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
 }
