204d13e3e89fa37119f82b1775e1024feda3611b
pabcavper <gelves.mula@gmail.com>
2020-11-05 12:23:30 +0100

    Creado el metodo main de la clase B3_EJ1

--- Ejercicio1/src/B3_EJ1.java
+++ Ejercicio1/src/B3_EJ1.java
@@ -22,4 +22,13 @@ public class B3_EJ1 {
             }
         }
     }
+
+    public static void main(String[] args) {
+        try {
+            B3_EJ1 b3E1 = new B3_EJ1();
+            b3E1.executeMultiThreading();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
 }
