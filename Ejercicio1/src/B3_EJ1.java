d18892125e6b07af2af4891ff96dbb272fc52875
pabcavper <gelves.mula@gmail.com>
2020-11-05 12:26:54 +0100

    Creada la clase Cocinero que implementa la interfaz Runnable y añadidas nuevas variables a la clase Control

--- Ejercicio1/src/B3_EJ1.java
+++ Ejercicio1/src/B3_EJ1.java
@@ -1,10 +1,36 @@
 public class B3_EJ1 {
 
     public class Control {
+        public final byte bPLATOS_MAX = 5;
+        public volatile int idCocinero = 0;
+        public volatile int idComensal = 0;
 
+        public int getIdCocinero() {
+            return idCocinero;
+        }
+
+        public void setIdCocinero(int idCocinero) {
+            this.idCocinero = idCocinero;
+        }
+
+        public int getIdComensal() {
+            return idComensal;
+        }
+
+        public void setIdComensal(int idComensal) {
+            this.idComensal = idComensal;
+        }
     }
 
     Control control = new Control();
+
+    public class Cocinero implements Runnable {
+
+        @Override
+        public void run() {
+
+        }
+    }
 
     /**
      * Este metodo es el que vamos a utilizar para todos los programas de MultiThreading.
@@ -25,8 +51,8 @@ public class B3_EJ1 {
 
     public static void main(String[] args) {
         try {
-            B3_EJ1 b3E1 = new B3_EJ1();
-            b3E1.executeMultiThreading();
+            B3_EJ1 b3EJ1 = new B3_EJ1();
+            b3EJ1.executeMultiThreading();
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
