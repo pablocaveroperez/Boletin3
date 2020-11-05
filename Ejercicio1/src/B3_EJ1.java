3a489c73267b08132eadcfe22d59e661d27d1d9c
pabcavper <gelves.mula@gmail.com>
2020-11-05 12:21:47 +0100

    Creado el metodo executeMultiThreading

--- Ejercicio1/src/B3_EJ1.java
+++ Ejercicio1/src/B3_EJ1.java
@@ -5,4 +5,21 @@ public class B3_EJ1 {
     }
 
     Control control = new Control();
+
+    /**
+     * Este metodo es el que vamos a utilizar para todos los programas de MultiThreading.
+     * Sirve para lanzar los hilos correcpondientes.
+     */
+    private void executeMultiThreading() {
+        int iContador = 0;
+
+        while (true) {
+            iContador++;
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
