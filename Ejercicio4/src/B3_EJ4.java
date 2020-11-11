1a854868c561761f82c92cc3e4c9b9b83ac2d70d
Pablo Cavero Perez <gelves.mula@gmail.com>
2020-11-11 19:07:12 +0100

    Estamos en ello

--- Ejercicio4/src/B3_EJ4.java
+++ Ejercicio4/src/B3_EJ4.java
@@ -46,25 +46,86 @@ public class B3_EJ4 {
 
     private Control control = new Control();
 
+    public class Dentista implements Runnable {
+
+        @Override
+        public void run() {
+            int idPaciente = 0;
+            Paciente p = new Paciente(idPaciente);
+            System.out.println("Hay " + NUM_SILLAS + " sillas libres");
+            System.out.println("El dentista esta libre.");
+            do {
+
+                    control.semaforoSillas.release();
+
+                    try {
+                        control.semaforoDentista.acquire();
+                        control.semaforoPaciente.acquire();
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+
+                    int iPos = control.colaPacientes.poll().getiId();
+                    System.out.println("El dentista ha llamado al paciente " + iPos);
+                    System.out.println("El dentista esta ocupado.");
+                    System.out.println("La silla ocupada por " + iPos + " esta libre.");
+
+
+
+                    try {
+                        Thread.sleep((int) (Math.random() * 10) * 1000);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+
+                    System.out.println("El paciente " + iPos + " ha salido de la consulta.");
+                    System.out.println("El dentista esta libre de nuevo.");
+
+                    control.semaforoDentista.release();
+
+
+            } while (true);
+
+        }
+
+    }
+
     public class Paciente implements Runnable {
         private int iId;
 
         public Paciente(int iId) {
+            setiId(iId);
+        }
+
+        public int getiId() {
+            return iId;
+        }
+
+        public void setiId(int iId) {
             this.iId = iId;
         }
 
         @Override
         public synchronized void run() {
 
+            if (control.colaPacientes.size() >= NUM_SILLAS) {
+                System.out.println("El paciente " + getiId() + " se va enfadado, por que la sala de espera estaba llena.");
+            } else {
+                try {
+                    control.semaforoSillas.acquire();
+                    System.out.println("El paciente " + getiId() + " se ha sentado en una silla.");
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                control.colaPacientes.add(this);
+                control.semaforoPaciente.release();
+            }
         }
     }
 
-    public class Dentista implements Runnable {
-        @Override
-        public synchronized void run() {
-
-        }
-    }
 
     private void executeMultiThreading() throws InterruptedException {
         int iContador = 0;
@@ -85,7 +146,5 @@ public class B3_EJ4 {
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
-
-
     }
 }
