95b96e759bc7ec642040053a03a7d8f8b08a6820
Pablo Cavero Perez <gelves.mula@gmail.com>
2020-11-12 22:08:04 +0100

    Aqui llegó Victor

--- Ejercicio2/src/B3_EJ2.java
+++ Ejercicio2/src/B3_EJ2.java
@@ -7,9 +7,39 @@ public class B3_EJ2 {
     private final int NUM_ESTUDIANTES = 5;
 
     public class Control {
-        private Semaphore semaforoPalillos = new Semaphore(NUM_PALILLOS);
-        private Semaphore semaphoreEstudiantes = new Semaphore(0);
+        private Semaphore[] aSemaforo = new Semaphore[NUM_PALILLOS];
+        private Semaphore semaphoreFilosofo = new Semaphore(0);
+        private Semaphore semaforoPalillo = new Semaphore(2);
         private Queue<Filosofo> colaFilosofos = new LinkedList<Filosofo>();
+
+        public Control() {
+            for (int i = 0; i < NUM_PALILLOS; i++)
+                aSemaforo[i] = new Semaphore(1);
+        }
+
+        public Semaphore[] getaSemaforo() {
+            return aSemaforo;
+        }
+
+        public void setaSemaforo(Semaphore[] aSemaforo) {
+            this.aSemaforo = aSemaforo;
+        }
+
+        public Semaphore getSemaphoreFilosofo() {
+            return semaphoreFilosofo;
+        }
+
+        public void setSemaphoreFilosofo(Semaphore semaphoreFilosofo) {
+            this.semaphoreFilosofo = semaphoreFilosofo;
+        }
+
+        public Queue<Filosofo> getColaFilosofos() {
+            return colaFilosofos;
+        }
+
+        public void setColaFilosofos(Queue<Filosofo> colaFilosofos) {
+            this.colaFilosofos = colaFilosofos;
+        }
     }
 
     private Control control = new Control();
@@ -30,8 +60,12 @@ public class B3_EJ2 {
         }
 
         @Override
-        public void run() {
-
+        public synchronized void run() {
+            System.out.println("El filósofo " + getiId() + " se ha sentado en el parque. No le teme a los \"Bichos\"");
+            do {
+                control.colaFilosofos.add(this);
+                control.semaphoreFilosofo.release();
+            }while (true);
         }
     }
 
@@ -51,27 +85,55 @@ public class B3_EJ2 {
         }
 
         @Override
-        public void run() {
+        public synchronized void run() {
+
             do {
                 try {
-                    control.semaforoPalillos.acquire();
-                    control.semaphoreEstudiantes.acquire();
 
-                    ;
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                try {
+                    control.semaphoreFilosofo.acquire();
+
+                    int iBichos = (int) (Math.random() * 52);
+
+                    int idFilosofo = control.colaFilosofos.poll().getiId();
+                    if (iBichos == 1)
+                        System.out.println("El filósofo " + idFilosofo + " está listo para comer. Le ha picado " + iBichos + " bicho");
+                    else
+                        System.out.println("El filósofo " + idFilosofo + " está listo para comer. Le han picado " + iBichos + " bichon");
+
+                    int iIzquierda = (int) (Math.random() * 5);
+                    int iDerecha;
+
+                    if (iIzquierda == 4)
+                        iDerecha = 0;
+                    else
+                        iDerecha = iIzquierda + 1;
+
+
+
+                    control.semaforoPalillo.acquire();
+
+                    control.aSemaforo[iIzquierda].acquire();
+                    control.aSemaforo[iDerecha].acquire();
+
+
+                    System.out.println("El filósofo " + idFilosofo + " ha cogido los palillos " + iIzquierda + " y " + iDerecha + ".");
+                    System.out.println("El filósofo " + idFilosofo + " ha empezado a comer.");
                     Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                int idFilosofo = control.colaFilosofos.poll().getiId();
-                System.out.println("El filósofo " + idFilosofo + " ha terminado de comer.");
 
-                System.out.println("Los palillos " + getiId() + " y " + (getiId()+1) + " estan libres.");
-                try {
+
+                    control.aSemaforo[iIzquierda].release();
+                    control.aSemaforo[iDerecha].release();
+
+                    control.semaforoPalillo.release();
+
+                    System.out.println("El filósofo " + idFilosofo + " ha terminado de comer. Y se dispone a pensar en el \"Bicho\"");
+                    System.out.println("Los palillos " + iIzquierda + " y " + iDerecha + " estan libres.");
+
+
+
+
                     Thread.sleep(6000);
+                    System.out.println("El filósofo " + idFilosofo + " ya ha dejado de pensar. Este cree en el \"Bicho\".");
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -84,15 +146,13 @@ public class B3_EJ2 {
      * Sirve para lanzar los hilos correcpondientes.
      */
     private void executeMultiThreading() throws InterruptedException {
-        int iContador = 0;
 
-        for (int i = 0; i < NUM_PALILLOS; i++) {
-            new Thread(new Palillo(i)).start();
-        }
-
-        for (int i = 0; i < NUM_ESTUDIANTES; i++) {
+        for (int i = 0; i < NUM_ESTUDIANTES; i++)
             new Thread(new Filosofo(i)).start();
-        }
+
+        for (int i = 0; i < NUM_PALILLOS; i++)
+            new Thread(new Palillo(i)).start();
+
     }
 
     public static void main(String[] args) {
