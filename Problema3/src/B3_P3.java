b0101f83c40aeaa2f2b8697db6533c60da61e81b
pabcavper <gelves.mula@gmail.com>
2020-11-19 09:00:55 +0100

    Todos los objetos Control modificados para que sean final

--- Problema3/src/B3_P3.java
+++ Problema3/src/B3_P3.java
@@ -1,16 +1,58 @@
-public class B3_P3 {
-    public class Control {
+import java.util.concurrent.Semaphore;
 
+public class B3_P3 {
+    private final int ESTUDIANTES = 3;
+
+    public class Control {
+        private Semaphore semaforo = new Semaphore(0);
+
+
+        public Semaphore getSemaforo() {
+            return semaforo;
+        }
+
+        public void setSemaforo(Semaphore semaforo) {
+            this.semaforo = semaforo;
+        }
     }
 
     private final Control control = new Control();
+
+    public class Estudiante implements Runnable {
+        private int iId;
+
+        public Estudiante(int iId) {
+            setiId(iId);
+        }
+
+        public int getiId() {
+            return iId;
+        }
+
+        public void setiId(int iId) {
+            this.iId = iId;
+        }
+
+        @Override
+        public void run() {
+
+        }
+    }
+
+    public class Malboro implements Runnable {
+
+        @Override
+        public void run() {
+
+        }
+    }
 
     private void executeMultiThreading() throws InterruptedException {
         for (int i = 0; i < ESTUDIANTES; i++) {
             new Thread(new Estudiante(i)).start();
         }
 
-        new Thread(new Cocinero()).start();
+        new Thread(new Malboro()).start();
     }
 
     public static void main(String[] args) {
