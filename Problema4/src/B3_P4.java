cb01a9a09ffa2ac11d73870744e1ebf0f3f6b2a6
Pablo Cavero Perez <gelves.mula@gmail.com>
2020-11-19 21:54:37 +0100

    Estamos trabajando en ello

--- Problema4/src/B3_P4.java
+++ Problema4/src/B3_P4.java
@@ -8,86 +8,51 @@ public class B3_P4 {
     private final byte BAJA = 0;
 
     public class Control {
-        private Semaphore semaforoArriba = new Semaphore(10);
-        private Semaphore semaforoAbajo = new Semaphore(10);
-        private Queue<Sevilla> colaSube = new LinkedList<Sevilla>();
-        private Queue<Betis> colaBaja = new LinkedList<Betis>();
-        private byte genteBajando = 0;
-        private byte genteSubiendo = 0;
-        private boolean subirPosible = false;
-        private boolean bajarPosible = false;
+        private Semaphore semaforoBaja = new Semaphore(10);
+        private Semaphore semaforoSube = new Semaphore(10);
+        private Queue<Estudiante> colaSube = new LinkedList<Estudiante>();
+        private Queue<Estudiante> colaBaja = new LinkedList<Estudiante>();
 
-        public boolean isSubirPosible() {
-            return subirPosible;
-        }
-
-        public void setSubirPosible(boolean subirPosible) {
-            this.subirPosible = subirPosible;
-        }
-
-        public boolean isBajarPosible() {
-            return bajarPosible;
-        }
-
-        public void setBajarPosible(boolean bajarPosible) {
-            this.bajarPosible = bajarPosible;
-        }
-
-        public byte getGenteBajando() {
-            return genteBajando;
-        }
-
-        public void setGenteBajando(byte genteBajando) {
-            this.genteBajando = genteBajando;
-        }
-
-        public byte getGenteSubiendo() {
-            return genteSubiendo;
-        }
-
-        public void setGenteSubiendo(byte genteSubiendo) {
-            this.genteSubiendo = genteSubiendo;
-        }
-
-        public Queue<Sevilla> getColaSube() {
+        public Queue<Estudiante> getColaSube() {
             return colaSube;
         }
 
-        public void setColaSube(Queue<Sevilla> colaSube) {
+        public void setColaSube(Queue<Estudiante> colaSube) {
             this.colaSube = colaSube;
         }
 
-        public Queue<Betis> getColaBaja() {
+        public Queue<Estudiante> getColaBaja() {
             return colaBaja;
         }
 
-        public void setColaBaja(Queue<Betis> colaBaja) {
+        public void setColaBaja(Queue<Estudiante> colaBaja) {
             this.colaBaja = colaBaja;
         }
 
-        public Semaphore getSemaforoArriba() {
-            return semaforoArriba;
+        public Semaphore getSemaforoBaja() {
+            return semaforoBaja;
         }
 
-        public void setSemaforoArriba(Semaphore semaforoArriba) {
-            this.semaforoArriba = semaforoArriba;
+        public void setSemaforoBaja(Semaphore semaforoBaja) {
+            this.semaforoBaja = semaforoBaja;
         }
 
-        public Semaphore getSemaforoAbajo() {
-            return semaforoAbajo;
+        public Semaphore getSemaforoSube() {
+            return semaforoSube;
         }
 
-        public void setSemaforoAbajo(Semaphore semaforoAbajo) {
-            this.semaforoAbajo = semaforoAbajo;
+        public void setSemaforoSube(Semaphore semaforoSube) {
+            this.semaforoSube = semaforoSube;
         }
     }
 
     private final Control control = new Control();
 
-    public class Sevilla implements Runnable {
+    public class Estudiante implements Runnable {
         private int iId;
+        private byte bTrambolico;
 
-        public Sevilla(int iId) {
+        public Estudiante(int iId) {
             setiId(iId);
         }
 
@@ -99,89 +64,50 @@ public class B3_P4 {
             this.iId = iId;
         }
 
+        public byte getbTrambolico() {
+            return bTrambolico;
+        }
+
+        public void setbTrambolico(byte bTrambolico) {
+            this.bTrambolico = bTrambolico;
+        }
+
         @Override
         public synchronized void run() {
             try {
-                control.semaforo.acquire();
-                control.colaSube.add(this);
-                if ((control.colaBaja.isEmpty() && control.genteSubiendo < 10) || !control.bajarPosible) {
-                    control.subirPosible = true;
-                    control.bajarPosible = false;
-                    System.out.println("El Sevillista " + getiId() + " se dispone a subir");
-                    control.genteSubiendo++;
-                    Thread.sleep(2000);
-                    System.out.println("El Sevillista " + getiId() + " ha subido");
+                setbTrambolico((byte) (Math.random() * 2));
+                if (getbTrambolico() == SUBE) {
+                    System.out.println("El estudiante " + getiId() + " esta esperando para subir.");
+                    control.colaSube.add(this);
+                    control.semaforoSube.acquire();
+                }else {
+                    System.out.println("El estudiante " + getiId() + " esta esperando para bajar.");
+                    control.colaBaja.add(this);
+                    control.semaforoBaja.acquire();
                 }
-
-                if (control.genteSubiendo == 10) {
-                    System.out.println("Ya han subido 10 sevillistas.");
-                    control.genteBajando = 0;
-                    for (int i = 0; i < 10; i++)
-                        control.colaBaja.poll();
-                    control.bajarPosible = true;
-                    control.subirPosible = false;
-                    control.semaforoAbajo.release(10);
-                    Thread.sleep(2000);
-                }
+                System.out.println("El estudiante " + getiId() + " ya ha salido de la escalera.");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
     }
 
-    public class Betis implements Runnable {
-        private int iId;
-
-        public Betis(int iId) {
-            setiId(iId);
-        }
-
-        public int getiId() {
-            return iId;
-        }
-
-        public void setiId(int iId) {
-            this.iId = iId;
-        }
+    public class Escalera implements Runnable {
 
         @Override
         public synchronized void run() {
-            try {
-                control.semaforo.acquire();
-                control.colaBaja.add(this);
-                if ((control.colaSube.isEmpty() && control.genteBajando < 10) || !control.subirPosible) {
-                    control.bajarPosible = true;
-                    control.subirPosible = false;
-                    System.out.println("El Betico " + getiId() + " se dispone a bajar. Jiji");
-                    control.genteBajando++;
-                    Thread.sleep(2000);
-                    System.out.println("El Betico " + getiId() + " ha bajado. Jiji");
-                }
-                if (control.genteBajando == 10) {
-                    System.out.println("Ya han bajado 10 beticos.");
-                    control.genteBajando = 0;
-                    for (int i = 0; i < 10; i++)
-                        control.colaBaja.poll();
-                    control.subirPosible = true;
-                    control.bajarPosible = false;
-                    control.semaforoArriba.release(10);
-                    Thread.sleep(2000);
-                }
 
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
         }
     }
 
     private void executeMultiThreading() throws InterruptedException {
         int iContador = 0;
+
+        new Thread(new Escalera()).start();
+
         while (true) {
+            new Thread(new Estudiante(iContador)).start();
             Thread.sleep(150);
-            new Thread(new Sevilla(iContador)).start();
-            iContador++;
-            Thread.sleep(150);
-            new Thread(new Betis(iContador)).start();
             iContador++;
         }
     }
