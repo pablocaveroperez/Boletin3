33aaa9a6b1ab3ef929b73efe41fe7b7ef52f2383
pabcavper <gelves.mula@gmail.com>
2020-11-19 09:24:07 +0100

    getters y setter de la clase control

--- Problema3/src/B3_P3.java
+++ Problema3/src/B3_P3.java
@@ -4,25 +4,78 @@ public class B3_P3 {
     private final int ESTUDIANTES = 3;
 
     public class Control {
-        private Semaphore semaforo = new Semaphore(0);
+        private Semaphore semaforoMalboro = new Semaphore(0);
+        private Semaphore semaforoEstudiante = new Semaphore(0);
+        private volatile int iIngrediente;
+        private volatile int iTabaco;
+        private volatile int iPapel;
+        private volatile int iCerillas;
 
-
-        public Semaphore getSemaforo() {
-            return semaforo;
+        public Semaphore getSemaforoMalboro() {
+            return semaforoMalboro;
         }
 
-        public void setSemaforo(Semaphore semaforo) {
-            this.semaforo = semaforo;
+        public void setSemaforoMalboro(Semaphore semaforoMalboro) {
+            this.semaforoMalboro = semaforoMalboro;
+        }
+
+        public Semaphore getSemaforoEstudiante() {
+            return semaforoEstudiante;
+        }
+
+        public void setSemaforoEstudiante(Semaphore semaforoEstudiante) {
+            this.semaforoEstudiante = semaforoEstudiante;
+        }
+
+        public int getiIngrediente() {
+            return iIngrediente;
+        }
+
+        public void setiIngrediente(int iIngrediente) {
+            this.iIngrediente = iIngrediente;
+        }
+
+        public int getiTabaco() {
+            return iTabaco;
+        }
+
+        public void setiTabaco(int iTabaco) {
+            this.iTabaco = iTabaco;
+        }
+
+        public int getiPapel() {
+            return iPapel;
+        }
+
+        public void setiPapel(int iPapel) {
+            this.iPapel = iPapel;
+        }
+
+        public int getiCerillas() {
+            return iCerillas;
+        }
+
+        public void setiCerillas(int iCerillas) {
+            this.iCerillas = iCerillas;
         }
     }
 
     private final Control control = new Control();
 
     public class Estudiante implements Runnable {
+        private int iIngrediente;
         private int iId;
 
         public Estudiante(int iId) {
             setiId(iId);
+        }
+
+        public int getiIngrediente() {
+            return iIngrediente;
+        }
+
+        public void setiIngrediente(int iIngrediente) {
+            this.iIngrediente = iIngrediente;
         }
 
         public int getiId() {
