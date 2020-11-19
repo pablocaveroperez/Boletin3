677e59cd7101818087f7117f7dc229b2b480125e
Pablo Cavero Perez <gelves.mula@gmail.com>
2020-11-19 20:23:25 +0100

    Estamos trabajando en ello

--- Problema5/src/B3_P5.java
+++ Problema5/src/B3_P5.java
@@ -1,22 +1,83 @@
-public class B3_P5 {
-    public class Control {
+import java.util.concurrent.Semaphore;
 
+public class B3_P5 {
+    private final byte GOOGLEMEETS = 0;
+    private final byte DISCORD = 1;
+
+    public class Control {
+        private Semaphore semaforoGoogleMeets = new Semaphore(0);
+        private Semaphore semaforoDiscord = new Semaphore(0);
+
+        public Semaphore getSemaforoGoogleMeets() {
+            return semaforoGoogleMeets;
+        }
+
+        public void setSemaforoGoogleMeets(Semaphore semaforoGoogleMeets) {
+            this.semaforoGoogleMeets = semaforoGoogleMeets;
+        }
+
+        public Semaphore getSemaforoDiscord() {
+            return semaforoDiscord;
+        }
+
+        public void setSemaforoDiscord(Semaphore semaforoDiscord) {
+            this.semaforoDiscord = semaforoDiscord;
+        }
     }
 
     private final Control control = new Control();
 
-    public class Urelio {
+    public class Urelio implements Runnable {
 
+        @Override
+        public synchronized void run() {
+            do {
+
+            }while (true);
+        }
+    }
+
+    public class Estudiante implements Runnable {
+        private int iId;
+        private byte bPlataforma;
+
+        public Estudiante(int iId) {
+            setiId(iId);
+        }
+
+        public int getiId() {
+            return iId;
+        }
+
+        public void setiId(int iId) {
+            this.iId = iId;
+        }
+
+        public int getbPlataforma() {
+            return bPlataforma;
+        }
+
+        public void setbPlataforma(byte bPlataforma) {
+            this.bPlataforma = bPlataforma;
+        }
+
+        @Override
+        public synchronized void run() {
+            setbPlataforma((byte) (Math.random() * 2));
+        }
     }
 
     private void executeMultiThreading() throws InterruptedException {
         int iContador = 0;
-        new Thread(new Aurelinex()).start();
+        new Thread(new Urelio()).start();
 
         while (true) {
             Thread.sleep(5000);
-            int i = (int) (1 + Math.random() * PREMIUM);
-            new Thread(new DamAir(i, iContador)).start();
+            new Thread(new Estudiante(iContador)).start();
+            iContador++;
+            new Thread(new Estudiante(iContador)).start();
+            iContador++;
+            new Thread(new Estudiante(iContador)).start();
             iContador++;
         }
     }
