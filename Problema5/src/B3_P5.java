46148c564786ebc701cb8061ea93765ac371b918
Pablo Cavero Perez <gelves.mula@gmail.com>
2020-11-19 21:38:23 +0100

    Terminado el problema 5

--- Problema5/src/B3_P5.java
+++ Problema5/src/B3_P5.java
@@ -31,25 +31,34 @@ public class B3_P5 {
 
         private void atiendeDiscord() {
             control.semaforoDiscord.release();
+            System.out.println("Urelio ha atendido a un estudiante de Discord.");
         }
 
         private void atiendeGoogleMeets() {
             control.semaforoGoogleMeets.release();
+            System.out.println("Urelio ha atendido a un estudiante de Google Mets.");
         }
 
         @Override
         public synchronized void run() {
             do {
-                if (control.semaforoGoogleMeets.getQueueLength() > control.semaforoDiscord.getQueueLength()) {
-                    atiendeGoogleMeets();
-                } else if (control.semaforoGoogleMeets.getQueueLength() == control.semaforoDiscord.getQueueLength()) {
-                    byte bNum = (byte) (Math.random() * 2);
-                    if (bNum == DISCORD) {
-                        atiendeDiscord();
-                    }else
+                try {
+                    Thread.sleep(250);
+                    if (control.semaforoGoogleMeets.getQueueLength() > control.semaforoDiscord.getQueueLength()) {
                         atiendeGoogleMeets();
-                } else
-                    atiendeDiscord();
+                    } else if (control.semaforoGoogleMeets.getQueueLength() == control.semaforoDiscord.getQueueLength()) {
+                        byte bNum = (byte) (Math.random() * 2);
+                        if (bNum == DISCORD) {
+                            atiendeDiscord();
+                        }else
+                            atiendeGoogleMeets();
+                    } else
+                        atiendeDiscord();
+
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }while (true);
         }
     }
@@ -83,10 +92,13 @@ public class B3_P5 {
             setbPlataforma((byte) (Math.random() * 2));
             try {
                 if (getbPlataforma() == DISCORD) {
+                    System.out.println("El estudiante " + getiId() + " ha entrado en Discord.");
                     control.semaforoDiscord.acquire();
                 }else {
+                    System.out.println("El estudiante " + getiId() + " ha entrado en Google Mets.");
                     control.semaforoGoogleMeets.acquire();
                 }
+                System.out.println("El estudiante " + getiId() + " ha sido atendido por Urelio. No está contento.");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -99,13 +111,13 @@ public class B3_P5 {
         new Thread(new Urelio()).start();
 
         while (true) {
+            new Thread(new Estudiante(iContador)).start();
+            iContador++;
+            new Thread(new Estudiante(iContador)).start();
+            iContador++;
+            new Thread(new Estudiante(iContador)).start();
+            iContador++;
             Thread.sleep(5000);
-            new Thread(new Estudiante(iContador)).start();
-            iContador++;
-            new Thread(new Estudiante(iContador)).start();
-            iContador++;
-            new Thread(new Estudiante(iContador)).start();
-            iContador++;
         }
     }
 
